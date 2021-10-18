759ffefbb5a370a349f526ebdb2646171e740c6f
EON-LEE <43924657+EON-LEE@users.noreply.github.com>
2021-10-18 19:19:36 +0900

    Create week7_1.sh

---
+++ week7/week7_1.sh
@@ -0,0 +1,15 @@
+mkdir git_project
+cd git_project
+git init
+git config --global user.name yourname
+git config --global user.email youremail
+echo hello world > test.sh
+git add test.sh
+git commit -m "Initial commit"
+echo hello new world >> test.sh
+echo hello git project > test2.sh
+git add .
+git commit -m "modify test.sh, make test2.sh"
+echo commit3 > test3.sh
+git add .
+git commit -m "3rd commit : make new shell script"
